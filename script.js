62123dafa53402572ba0f3dd2d0c15cebbfbbbab
SoloGudLyf <solomonteiko0@gmail.com>
2025-10-05 00:47:15 +0000

    feat: create function to get computer's choice

---
+++ script.js
@@ -0,0 +1,16 @@
+function getComputerChoice() {
+  let computerChoice = Math.floor(Math.random() * 3);
+
+  if (computerChoice === 0) {
+    computerChoice = "rock";
+  } else if (computerChoice === 1) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+  return computerChoice;
+}
+
+console.log(getComputerChoice());
+
+
